3580abb6e9c351c1b69de06c942caaf4feb6e4f5
lucaq <lucaq@LUCAPC.fritz.box>
2021-04-26 18:29:57 +0200

    All Files

---
+++ CosmeticsCommand/src/de/infernoxx/cosmeticscommand/commands/AliasCommand.java
@@ -0,0 +1,20 @@
+package de.infernoxx.cosmeticscommand.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class AliasCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(sender instanceof Player) {
+			Player player = (Player) sender;
+			player.chat("/pc open main");
+			
+		}
+		return false;
+	}
+
+}
